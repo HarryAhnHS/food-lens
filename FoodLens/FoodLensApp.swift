5014f30685932345b52f341f553e2d0552e1b30b
Hongsuh Ahn <hongsuhahn@gmail.com>
2024-12-09 22:00:01 -0800

    Initial Commit

---
+++ FoodLens/FoodLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLensApp.swift
+//  FoodLens
+//
+//  Created by Harry Ahn on 9/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
